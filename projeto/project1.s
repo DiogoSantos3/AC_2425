2212343c5bd906199c6f38f9afbcf5a259d47209
diogosantos <dpsantos005@gmail.com>
2025-05-23 09:46:11 +0100

    d

--- projeto/project1.s
+++ projeto/project1.s
@@ -1,4 +1,4 @@
-	.text
+.text
 	b		program
 	b 		.	   
 program:
@@ -7,10 +7,15 @@ program:
 addr_stack_top:
 	.word 	stack_top
 
+    ;constants
+         .equ	INPORT_ADDRESS, 0xFF80
+	     .equ	OUTPORT_ADDRESS, 0xC0FF
+
+
 
 main:
-    b read_sides
-    b       .
+
+
 
 rand_seed_addr:
     .word rand_seed
@@ -19,11 +24,7 @@ rand_seed_addr:
 ; uint8_t read_sides()
 read_sides:
     
-    mov r1, #0x80
-    movt r1, #0xFF
-
-    ; lê o bit do DIP-switch
-    ldrb r0, [r1]  ; r0= valor lido
+    bl _Inport_Read ; Lê o valor do inport
 
     ;Isola os bits 2 e 3
     mov r1, #0x0C     ; coloca 0x0C em r1
@@ -36,12 +37,16 @@ read_sides:
     ;retorna em r0 (0 = 4 lados, 1 = 6 lados, 2 = 8 lados, 3 = 12lados)
     mov pc, lr
 
-; =============================================================================
-; Dados e variáveis globais
-; =============================================================================
+
+ _Inport_Read:
+mov r1, #INPORT_ADDRESS & 0xFF
+movt r1, #(INPORT_ADDRESS >> 8) & 0xFF
+ldrb r0, [ r1, #0]
+mov pc, lr
+
+
     .data
 
 roll_flag:     .word 0       ; Flag de controlo de ROLL
 rand_seed:     .word 1       ; Semente do gerador pseudo-aleatório
-
-
+stack_top:  
